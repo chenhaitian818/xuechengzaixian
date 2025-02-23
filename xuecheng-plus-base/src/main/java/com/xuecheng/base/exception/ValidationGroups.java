f117926a0bac0faf104039d7a2c1e5fe2fd9cb42
chenhaitian <3132701541@qq.com>
2025-02-23 17:11:06 +0800

    DAY3 JSR303使用

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/ValidationGroups.java
@@ -0,0 +1,14 @@
+package com.xuecheng.base.exception;
+
+/**
+ * @Author chenhaitian
+ * @Packge com.xuecheng.base.exception
+ * @Date 2025/2/23 16:10
+ * @description 校验分组
+ */
+public class ValidationGroups {
+    public interface Insert{};
+    public interface Update{};
+    public interface Delete{};
+
+}
